75b9ae091f1d9a1b6caa8027eee5ae62c7d6d55e
Tdnzt <thi.denizet@gmail.com>
2013-09-10 22:16:11 +0700

    Implemented the models Animal and Tribe.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,12 @@
 class Animal < ActiveRecord::Base
+	belongs_to :tribe
+	inheritance_column = :race
+
+	scope :lions, -> { where(race: 'Lion') }
+	scope :meerkats, -> { where(race: 'Meerkat') }
+	scope :wild_boars, -> { where(race: 'WildBoar') }
 end
+
+class Lion < Animal; end
+class Meerkat < Animal; end
+class WildBoar < Animal; end
--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -1,2 +1,4 @@
 class Tribe < ActiveRecord::Base
+	has_many :animals
+	delegate :lions, :meerkats, :wild_boards, to: :animals
 end
