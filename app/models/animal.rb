db7769790d6f6da8695ad0a765ddc7bb399c34bc
Tn6o <tn6o.dev@gmail.com>
2014-04-10 00:26:44 +0700

    Validates the presence of the race to avoid the creation of animals without a race.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,8 @@ class Animal < ActiveRecord::Base
 	belongs_to :tribe
 	self.inheritance_column = :race
 
+    validates :race, presence: true
+
 	scope :lions, -> { where(race: 'Lion') }
 	scope :meerkats, -> { where(race: 'Meerkat') }
 	scope :wild_boars, -> { where(race: 'WildBoar') }
