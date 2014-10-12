085a65da32812ecd8be6fb3f30ff5ef12b8dcce3
T-Dnzt <thi.denizet@gmail.com>
2014-10-12 15:01:59 +0700

    Add some seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create a lion with with the Animal model
+Animal.create!(name: 'Simba', race: 'Lion', age: 3)
+
+# Create a lion with with the Lion model
+Lion.create!(name: 'Nala', age: 3)
+
+# Create a Wild Boar with with the WildBoar model
+WildBoar.create!(name: 'Pumba', age: 6)
+
+# Create a Meerkat with with the Meerkat model
+Meerkat.create!(name: 'Timon', age: 6)
+
